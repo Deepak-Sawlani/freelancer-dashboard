1ccc97133018ceaca3291ebb26b9e2afe07b6b32
Deepak Sawlani <deepaksawlani2003@gmail.com>
2026-05-03 12:28:24 +0530

    Remove all files from GitHub, keep locally

--- backend/models/Project.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const projectSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  githubLink: String,
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
-module.exports = mongoose.model('Project', projectSchema);
